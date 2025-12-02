e892cf911d67f546707127b94df359dda610a69c
Matteo Fernandes <mfernand@f1br8s18.clusters.42paris.fr>
2025-12-02 13:30:54 +0100

    sauvew la peau de lours

--- tester.py
+++ tester.py
@@ -1,13 +1,14 @@
 from predict import linearRegression, load_t
 from train import db, train
-import csv
+import numpy as np
+
 
 train()
 
 t0, t1 = load_t()
 print("t0: ", t0, "t1: ", t1)
 
-test_mileages = [15000, 25000, 35000]
+test_mileages = [50000, 25000, 35000]
 for m in test_mileages:
     print(f"Mileage: {m}, Estimated price: {linearRegression(m, t0, t1)}")
 
@@ -22,4 +23,7 @@ plt.xlabel("mileage")
 plt.ylabel("price")
 plt.title("Price/Mileage")
 plt.grid(True, alpha=0.3)
+x_line = np.array([db["km"].min(), db["km"].max()])
+y_line = linearRegression(x_line, t0, t1)
+plt.plot(x_line, y_line, color="red", label=f"y = {t0:.1f} + {t1:.5f} x")
 plt.show()
--- train.py
+++ train.py
@@ -1,3 +1,41 @@
+# import pandas as pd
+# import csv
+# from predict import linearRegression
+
+# t0 = 0.0
+# t1 = 0.0
+
+# db = pd.read_csv("./data.csv")
+
+
+# # db["km"] = db["km"] # Normalize
+# # db["price"] = db["price"]
+
+# learning_rate = 1e-6
+# iterations = 50000
+
+
+# def train():
+#     global t0, t1
+
+
+#     for _ in range(iterations):
+#         sum_t0 = 0.0
+#         sum_t1 = 0.0
+
+#         for mile, price in zip(db["km"], db["price"]):
+#             error = (linearRegression(mile, t0, t1) - price)
+#             sum_t0 += error
+#             sum_t1 += error * mile
+#         t0 -= learning_rate * 1/db.shape[0] * sum_t0
+#         t1 -= learning_rate * 1/db.shape[0] * sum_t1
+
+
+#     with open("thetaValue.csv", "w") as f:
+#         writer = csv.writer(f)
+#         writer.writerow([t0, t1])
+
+
 import pandas as pd
 import csv
 from predict import linearRegression
@@ -7,30 +45,38 @@ t1 = 0.0
 
 db = pd.read_csv("./data.csv")
 
+# Normalisation
+km_mean = db["km"].mean()
+km_std = db["km"].std()
+price_mean = db["price"].mean()
+price_std = db["price"].std()
 
-# db["km"] = db["km"] # Normalize
-# db["price"] = db["price"]
+db["km_norm"] = (db["km"] - km_mean) / km_std                           #x norm = x - x.mean() / x.std
+db["price_norm"] = (db["price"] - price_mean) / price_std
 
-learning_rate = 1e-12
-iterations = 40000
-
+learning_rate = 0.01
+iterations = 15000
 
 def train():
     global t0, t1
 
-
-    for _ in range(iterations):
+    for i in range(iterations):
         sum_t0 = 0.0
         sum_t1 = 0.0
 
-        for mile, price in zip(db["km"], db["price"]):
-            error = (linearRegression(mile, t0, t1) - price)
+        for km, price in zip(db["km_norm"], db["price_norm"]):
+            error = (linearRegression(km, t0, t1) - price)
             sum_t0 += error
-            sum_t1 += error * mile
-        t0 -= learning_rate * 1/db.shape[0] * sum_t0
-        t1 -= learning_rate * 1/db.shape[0] * sum_t1
+            sum_t1 += error * km
 
+        t0 -= learning_rate * (1 / db.shape[0]) * sum_t0
+        t1 -= learning_rate * (1 / db.shape[0]) * sum_t1
+
+    real_t1 = t1 * (price_std / km_std)
+    real_t0 = price_mean + (t0 * price_std) - real_t1 * km_mean
 
     with open("thetaValue.csv", "w") as f:
         writer = csv.writer(f)
-        writer.writerow([t0, t1])
+        writer.writerow([real_t0, real_t1])
+
+
